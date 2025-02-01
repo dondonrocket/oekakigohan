400a724b6cc58d931977a888cb316333be11b512
Nobutoshi Hasegawa <hasegawa12011@gmail.com>
2025-02-01 19:14:49 +0900

    favicon

--- resources/views/vue-app.blade.php
+++ resources/views/vue-app.blade.php
@@ -8,6 +8,9 @@
     <!-- フォントとBootstrapの読み込み -->
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Yomogi&display=swap" rel="stylesheet">
+    <!--ファビコン-->
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
