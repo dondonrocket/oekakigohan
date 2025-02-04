65b6586201e98b8841ef15c7b876df77bb461ed7
Nobutoshi Hasegawa <hasegawa12011@gmail.com>
2025-02-04 22:35:58 +0900

    hambargermenu check8

--- resources/views/vue-app.blade.php
+++ resources/views/vue-app.blade.php
@@ -83,7 +83,7 @@ main {
 .nav-link.custom-nav-link {
     color: #535353 !important;
     font-weight: bold;
-    font-size: 1rem;
+    font-size: 0.8rem;
     padding: 0.5rem 1rem;
     transition: color 0.3s ease;
 }
